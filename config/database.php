f4b8444c3d90d62e294a5112dc4eeca44a1a0bbd
mista <m.karim78@icloud.com>
2025-08-16 03:52:50 +0200

    Initial commit

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
             'port' => 3306,
             'database' => 'frame',
             'username' => 'root',
-            'password' => 'kmaoulida',
+            'password' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
